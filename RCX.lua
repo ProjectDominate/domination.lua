5e5fe6abcdf79f796ebba9c9a3bcd293b68ed982
ProjectDominate <101818820+ProjectDominate@users.noreply.github.com>
2022-05-20 16:12:04 +0200

    Update RCX.lua

--- RCX.lua
+++ RCX.lua
@@ -1,5 +1,6 @@
 getgenv().DominationRCX = {
      ["Working"] = true,
      ["Version"] = "0.9",
-     ["SmallLog"] = "Released, Adding Hatch Notifs soon!"
+     ["SmallLog"] = "Released, Adding Hatch Notifs soon!",
+     ["ActiveCodes"] = {"50m","hell","steampunk","100kthanks","easter2"}
 }
